04ab7139cce63b8863293017028266d9507ae21b
Axel GAILLIARD <>
2023-12-19 16:25:54 +0100

    Jump

--- Assets/Scripts/Player/JumpBehaviour.cs
+++ Assets/Scripts/Player/JumpBehaviour.cs
@@ -9,47 +9,12 @@ public class JumpBehaviour : MonoBehaviour
     public float jumpPower = 10.0f;
     public GroundCheck GroundCheck = null;
 
-    public InputActionReference clickActionRef = null;
-    public LayerMask groudLayer;
-    public Joystick joystick = null;
-
-
-    private void OnFingerDown(EnhancedTouch.Finger finger)
+    public void Jump()
     {
-        RectTransform joystikCoord = (joystick.transform as RectTransform);
-
-        if (finger.screenPosition.x > joystikCoord.offsetMin.x && finger.screenPosition.x < joystikCoord.offsetMax.x
-                       && finger.screenPosition.y > joystikCoord.offsetMin.y && finger.screenPosition.y < joystikCoord.offsetMax.y)
-        {
-            return;
-        }
-
         if (GroundCheck.isgrounded)
         {
-            Jump();
+            rb.AddForce(Vector3.up * jumpPower);
         }
-    }
-
-    private void Jump()
-    {
-        rb.AddForce(Vector3.up * jumpPower);
-    }
-
-    void OnEnable()
-    {
-        clickActionRef.action.performed += OnclickActionRef;
-        EnhancedTouch.TouchSimulation.Enable();
-        EnhancedTouch.EnhancedTouchSupport.Enable();
-        EnhancedTouch.Touch.onFingerDown += OnFingerDown;
-
-    }
-
-    void OnDisable()
-    {
-        clickActionRef.action.performed -= OnclickActionRef;
-        EnhancedTouch.TouchSimulation.Disable();
-        EnhancedTouch.EnhancedTouchSupport.Disable();
-        EnhancedTouch.Touch.onFingerDown -= OnFingerDown;
     }
 
 }
